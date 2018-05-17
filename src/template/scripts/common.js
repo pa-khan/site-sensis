5fd690f8f2dcd6ecc163f193c2cb4bf9c1a72483
e5ash <e5ash.bro@gmail.com>
2018-05-17 15:17:57 +0300

    100%

--- src/template/scripts/common.js
+++ src/template/scripts/common.js
@@ -1,6 +1,8 @@
 $(document).ready(function($) {
 
-	$('.input_phone .input__wrap').mask('+7 (000) 000-00-00');
+	$('.input_phone input').mask('+7 (000) 000-00-00');
 
-	
+	$('.about').tabs();	
+
+	new WOW().init();
 });
